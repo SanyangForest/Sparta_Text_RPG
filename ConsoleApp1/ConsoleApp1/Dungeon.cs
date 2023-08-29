762a3c654f76705c2fdcef8875f6a8761af67e5b
DoDokang318 <110431311+DoDokang318@users.noreply.github.com>
2023-08-29 12:46:13 +0900

    던전진입과 몬스터 랜덤등장 기능
    던전진입 화면에서
    배틀로 화면으로 가거나
     마을로 돌아갈수있는 기능
    
    "대포미니언", "미니언", "공허충"
    셋중  하나의 몬스터가 등장

---
+++ ConsoleApp1/ConsoleApp1/Dungeon.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Dungeon
+    {
+        Character newCharacter = new Character("Chad", "전사", 1, 10, 5, 100, 1500);
+         
+        internal void ChoiceDungeon()
+        {
+
+            Dungeon dungeon = new Dungeon();
+            Console.Clear();
+            Console.WriteLine("1. 마을");
+            Console.WriteLine("2. 베틀");
+            Console.WriteLine("진입하시겠습니까?");
+
+            int input = Program.CheckValidInput(1, 2);
+            switch (input)
+            {
+                case 0:
+                    Program.DisplayGameIntro();
+                    break;
+                case 1:
+                    dungeon.Battle();
+                    break;
+            }
+
+
+        }
+
+        public void Battle()
+        {
+            string[] monsterNames = { "대포미니언", "미니언", "공허충" };
+            Random random = new Random();
+
+            int randomIndex = random.Next(monsterNames.Length);
+            Monster newMonster = new Monster(monsterNames[randomIndex]);
+
+            Console.WriteLine($"{newMonster.Name}가 등장했다!");
+            newMonster.StatusRender(newMonster.Name);
+
+
+            Console.WriteLine("1. 때린다");
+            Console.WriteLine("2. 스킬");
+            Console.WriteLine("3. 도망간다");
+        }
+
+        enum STARTSELECT
+        {
+            SLELCTTOWN,
+            SLELCBETTLE,
+            NONSELECT
+
+        }
+
+       
+
+
+
+    }
+
+    class FightUnit
+    {
+        public String Name = "None";
+        protected int AT = 10;
+        public int HP = 50;
+        protected int MaxHp = 100;
+        protected int LV;
+
+
+        public void StatusRender(string _name)
+        {
+            Name = _name;
+            Console.WriteLine(Name);
+            Console.WriteLine("의능력치-----------------------------------------------");
+            Console.Write("공격력:");
+            Console.WriteLine(AT);
+            //50/100
+            Console.Write("체력:");
+            Console.Write(HP);
+            Console.Write("/");
+            Console.WriteLine(MaxHp);
+            Console.WriteLine("-----------------------------------------------");
+        }
+        public void BattleLogic(string _Name)
+        {
+
+
+            Console.WriteLine($"Lv.{LV} {Name} 의 공격!");
+            Console.WriteLine($" {_Name} 을(를) 맞췄습니다.  [데미지: {AT}]");
+
+            Console.WriteLine($"Lv.{LV} {Name}");
+            Console.WriteLine($"HP{HP}-> {HP -= AT}");
+            Console.WriteLine($"HP {HP}");
+
+
+        }
+
+
+    }
+
+
+   
+    class Monster : FightUnit
+    {
+
+        public string Name { get; }
+        public Monster(string _Name)
+        {
+
+            Name = _Name;
+            LV = 1;
+        }
+    }
+
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,122 +1,130 @@
-﻿internal class Program //커밋 해보기
+﻿namespace ConsoleApp1
 {
-    private static Character player;
-
-    static void Main(string[] args)
+    internal class Program //커밋 해보기
     {
-        GameDataSetting();
-        DisplayGameIntro();
-    }
 
-    static void GameDataSetting()
-    {
-        // 캐릭터 정보 세팅
-        player = new Character("Chad", "전사", 1, 10, 5, 100, 1500);
+        internal static Character player;
 
-        // 아이템 정보 세팅
-    }
-
-    static void DisplayGameIntro()
-    {
-        Console.Clear();
-
-        Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
-        Console.WriteLine("이곳에서 전전으로 들어가기 전 활동을 할 수 있습니다.");
-        Console.WriteLine();
-        Console.WriteLine("1. 상태보기");
-        Console.WriteLine("2. 인벤토리");
-        Console.WriteLine();
-        Console.WriteLine("원하시는 행동을 입력해주세요.");
-
-        int input = CheckValidInput(1, 2);
-        switch (input)
+        static void Main(string[] args)
         {
-            case 1:
-                DisplayMyInfo();
-                break;
-
-            case 2:
-                // 작업해보기
-                break;
+            GameDataSetting();
+            DisplayGameIntro();
         }
-    }
-    static void Shop()
-    {
-        Console.Clear();
-        Console.WriteLine("상점에 오신 것을 환영합니다 !")
-        Console.WriteLine("아래에 물건을 구매하실 수 있습니다.");
-        Console.WriteLine("현재 소지액: " + player.Gold);
-        
-    }
-    static void DisplayMyInfo()
-    {
-        Console.Clear();
 
-        Console.WriteLine("상태보기");
-        Console.WriteLine("캐릭터의 정보르 표시합니다.");
-        Console.WriteLine();
-        Console.WriteLine($"Lv.{player.Level}");
-        Console.WriteLine($"{player.Name}({player.Job})");
-        Console.WriteLine($"공격력 :{player.Atk}");
-        Console.WriteLine($"방어력 : {player.Def}");
-        Console.WriteLine($"체력 : {player.Hp}");
-        Console.WriteLine($"Gold : {player.Gold} G");
-        Console.WriteLine();
-        Console.WriteLine("0. 나가기");
-
-        int input = CheckValidInput(0, 0);
-        switch (input)
+        static void GameDataSetting()
         {
-            case 0:
-                DisplayGameIntro();
-                break;
+            // 캐릭터 정보 세팅
+            player = new Character("Chad", "전사", 1, 10, 5, 100, 1500);
+
+            // 아이템 정보 세팅
         }
-    }
 
-    static void DisplayInventory()
-    {
-
-    }
-
-    static int CheckValidInput(int min, int max)
-    {
-        while (true)
+        internal static void DisplayGameIntro()
         {
-            string input = Console.ReadLine();
+            Dungeon dungeon = new Dungeon();
 
-            bool parseSuccess = int.TryParse(input, out var ret);
-            if (parseSuccess)
+            Console.Clear();
+
+            Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
+            Console.WriteLine("이곳에서 전전으로 들어가기 전 활동을 할 수 있습니다.");
+            Console.WriteLine();
+            Console.WriteLine("1. 상태보기");
+            Console.WriteLine("2. 인벤토리");
+            Console.WriteLine("3. 던전");
+            Console.WriteLine();
+            Console.WriteLine("원하시는 행동을 입력해주세요.");
+
+            int input = CheckValidInput(1, 3);
+            switch (input)
             {
-                if (ret >= min && ret <= max)
-                    return ret;
-            }
+                case 1:
+                    DisplayMyInfo();
+                    break;
 
-            Console.WriteLine("잘못된 입력입니다.");
+                case 2:
+                    // 작업해보기
+                    break;
+                case 3:
+                    dungeon.ChoiceDungeon();
+                    break;
+            }
+        }
+        static void Shop()
+        {
+            Console.Clear();
+            Console.WriteLine("상점에 오신 것을 환영합니다 !");
+            Console.WriteLine("아래에 물건을 구매하실 수 있습니다.");
+            Console.WriteLine("현재 소지액: " + player.Gold);
+
+        }
+        static void DisplayMyInfo()
+        {
+            Console.Clear();
+
+            Console.WriteLine("상태보기");
+            Console.WriteLine("캐릭터의 정보르 표시합니다.");
+            Console.WriteLine();
+            Console.WriteLine($"Lv.{player.Level}");
+            Console.WriteLine($"{player.Name}({player.Job})");
+            Console.WriteLine($"공격력 :{player.Atk}");
+            Console.WriteLine($"방어력 : {player.Def}");
+            Console.WriteLine($"체력 : {player.Hp}");
+            Console.WriteLine($"Gold : {player.Gold} G");
+            Console.WriteLine();
+            Console.WriteLine("0. 나가기");
+
+            int input = CheckValidInput(0, 0);
+            switch (input)
+            {
+                case 0:
+                    DisplayGameIntro();
+                    break;
+            }
+        }
+
+        static void DisplayInventory()
+        {
+
+        }
+
+        internal static int CheckValidInput(int min, int max)
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                bool parseSuccess = int.TryParse(input, out var ret);
+                if (parseSuccess)
+                {
+                    if (ret >= min && ret <= max)
+                        return ret;
+                }
+
+                Console.WriteLine("잘못된 입력입니다.");
+            }
+        }
+    }
+
+
+    public class Character
+    {
+        public string Name { get; }
+        public string Job { get; }
+        public int Level { get; }
+        public int Atk { get; }
+        public int Def { get; }
+        public int Hp { get; }
+        public int Gold { get; }
+
+        public Character(string name, string job, int level, int atk, int def, int hp, int gold)
+        {
+            Name = name;
+            Job = job;
+            Level = level;
+            Atk = atk;
+            Def = def;
+            Hp = hp;
+            Gold = gold;
         }
     }
 }
-
-
-public class Character
-{
-    public string Name { get; }
-    public string Job { get; }
-    public int Level { get; }
-    public int Atk { get; }
-    public int Def { get; }
-    public int Hp { get; }
-    public int Gold { get; }
-
-    public Character(string name, string job, int level, int atk, int def, int hp, int gold)
-    {
-        Name = name;
-        Job = job;
-        Level = level;
-        Atk = atk;
-        Def = def;
-        Hp = hp;
-        Gold = gold;
-    }
-}
-
-//커밋해보기
